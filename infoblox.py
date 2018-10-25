b688d732f22b9c160bcd9d8ea16ccb4455970028
MikeP <mike@snowcrash.ca>
2018-10-25 14:46:39 -0400

    fix contributors:

--- infoblox.py
+++ infoblox.py
@@ -13,8 +13,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# Additions/alterations by "Mike Patterson" <mpatters@uwaterloo.ca>
-#  2015-2017
+# Additions/alterations by
+# Mike Patterson <mpatters@uwaterloo.ca>
+# Ryan Goggin
+# Steve Scott
+# all UW-IST-ISS
+# 2015-2018
 
 import re
 import requests
